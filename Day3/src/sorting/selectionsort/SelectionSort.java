6f5504cdfb3cb21063520eef31a6887a602ca93b
bhavyuh15 <bhavyadatey15@gmail.com>
2025-02-01 00:11:17 +0530

    Selection sort

---
+++ Day3/src/sorting/selectionsort/SelectionSort.java
@@ -0,0 +1,30 @@
+package sorting.selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] examScores = {85, 92, 78, 64, 89, 73, 99};
+        selectionSort(examScores);
+        System.out.println("Sorted Exam Scores: " + Arrays.toString(examScores));
+    }
+}
